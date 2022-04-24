692a52a2ec87358ba326449b965d306a12217616
Adinnu Benedict <benidixt@gmail.com>
2022-04-24 12:48:26 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    dispatch(function () {
+        sleep(15);
+
+        Mail::raw('plain text message', function ($message) {
+            $message->from('john@johndoe.com', 'John Doe');
+            $message->sender('john@johndoe.com', 'John Doe');
+            $message->to('john@johndoe.com', 'John Doe');
+            $message->cc('john@johndoe.com', 'John Doe');
+            $message->bcc('john@johndoe.com', 'John Doe');
+            $message->replyTo('john@johndoe.com', 'John Doe');
+            $message->subject('Subject');
+            $message->priority(3);
+        });
+    })->afterResponse();
+    return "Yes";
+});
+
+
+
+
+
 Route::fallback(function ($route) {
-    $data = Cache::rememberForever($route, function () use ($route) {
+    $data = Cache::rememberForever(Str::beforeLast($route, '/'), function () use ($route) {
         return file_get_contents('https://source.unsplash.com/random/300×300/?product');
     });
 
